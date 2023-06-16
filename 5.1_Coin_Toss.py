d3a12854e9e70bc84864f066e691a2bbb160a730
Shea <hermon.oshea@carlislecsd.org>
2023-06-16 18:13:12 -0500

    finished

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,7 +6,18 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
-
+import random
+heads = 0
+tails = 0
+for i in range(50):
+    flip = random.randrange(-1, 1)
+    if flip == 0:
+        print("Heads!")
+        heads+=1
+    else:
+        print("Tails!")
+        tails+=1
+print("The total heads you flipped was ", heads," The total number of tails you flipped was ", tails)
 
 
 
